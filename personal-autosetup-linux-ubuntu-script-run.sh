6c50ca656ab002cb33df6679eb94da35059c5bfe
Novimatrem <thenovimatrem@protonmail.ch>
2020-09-19 23:00:04 +0000

    Update personal-autosetup-linux-ubuntu-script-run.sh

--- personal-autosetup-linux-ubuntu-script-run.sh
+++ personal-autosetup-linux-ubuntu-script-run.sh
@@ -714,6 +714,9 @@ cd /opt
 wget https://github.com/cheesecakeufo/komorebi/releases/download/v2.1/komorebi-2.1-64-bit.deb
 sudo gdebi -n /opt/komorebi-2.1-64-bit.deb
 
+sudo apt-get install build-essential linux-headers-$(uname -r)
+ins virtualbox-guest-x11
+
 uppy
 
 sudo update-alternatives --all
