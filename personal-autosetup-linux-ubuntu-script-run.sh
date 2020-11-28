7f251ae96726c04c022180f02974f5ed66f346a0
Novimatrem <thenovimatrem@protonmail.ch>
2020-11-28 07:27:41 +0000

    Update personal-autosetup-linux-ubuntu-script-run.sh

--- personal-autosetup-linux-ubuntu-script-run.sh
+++ personal-autosetup-linux-ubuntu-script-run.sh
@@ -1042,6 +1042,7 @@ sudo apt remove -y catfish
 sudo apt remove -y xfce4-clipman
 sudo apt remove -y totem
 sudo apt remove -y gnome-music
+sudo apt remove -y deja-dup
 
 sudo dpkg --configure -a && sudo apt-get -f install && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y && sudo dpkg --configure -a && sudo apt-get -f install
 
