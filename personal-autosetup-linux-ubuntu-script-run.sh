c188b3dc5d18d349db503233a45200ece8c736c3
Novimatrem <thenovimatrem@protonmail.ch>
2021-02-10 00:08:22 +0000

    remove break

--- personal-autosetup-linux-ubuntu-script-run.sh
+++ personal-autosetup-linux-ubuntu-script-run.sh
@@ -1764,7 +1764,7 @@ wget https://gitlab.com/Novimatrem/personal-autosetup-linux-ubuntu/-/raw/master/
 
 touch /home/$(whoami)/.xbindkeysrc
 
-echo '"bash /opt/open-thunar-in-awesomewm.sh""' >> /home/$(whoami)/.xbindkeysrc
+echo '"bash /opt/open-thunar-in-awesomewm.sh' >> /home/$(whoami)/.xbindkeysrc
 echo -e ' \t Control + Alt + E' >> /home/$(whoami)/.xbindkeysrc
 
 xbindkeys_autostart
