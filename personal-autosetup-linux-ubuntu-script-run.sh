c6d595cbe8c373f23355c137725f66755bb3f363
Novimatrem <thenovimatrem@protonmail.ch>
2021-02-19 02:56:45 +0000

    okay fine i'll use the wine ppa even though it's always fkn down

--- personal-autosetup-linux-ubuntu-script-run.sh
+++ personal-autosetup-linux-ubuntu-script-run.sh
@@ -1896,6 +1896,25 @@ ins libsqlite3-0:i386
 ins msttcorefonts
 # /additional wine dependencies (WoW)
 
+# okay fine i'll use the wine ppa even though it's always fkn down
+sudo dpkg --add-architecture i386 
+
+sudo dpkg --configure -a && sudo apt-get -f install && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y && sudo dpkg --configure -a && sudo apt-get -f install && flatpak update -y --noninteractive && sudo flatpak update -y --noninteractive && flatpak uninstall --unused && sudo snap refresh && sudo journalctl --flush && sudo journalctl --rotate && sudo journalctl --vacuum-time=1s && sudo journalctl --vacuum-size=200M && sudo journalctl --flush
+
+sudo dpkg --add-architecture i386 
+
+cd /opt/
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+
+arepo 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' 
+
+sudo dpkg --configure -a && sudo apt-get -f install && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y && sudo dpkg --configure -a && sudo apt-get -f install && flatpak update -y --noninteractive && sudo flatpak update -y --noninteractive && flatpak uninstall --unused && sudo snap refresh && sudo journalctl --flush && sudo journalctl --rotate && sudo journalctl --vacuum-time=1s && sudo journalctl --vacuum-size=200M && sudo journalctl --flush
+
+sudo apt install -y --install-recommends winehq-devel
+
+sudo dpkg --configure -a && sudo apt-get -f install && sudo apt update -y && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y && sudo apt clean -y && sudo dpkg --configure -a && sudo apt-get -f install && flatpak update -y --noninteractive && sudo flatpak update -y --noninteractive && flatpak uninstall --unused && sudo snap refresh && sudo journalctl --flush && sudo journalctl --rotate && sudo journalctl --vacuum-time=1s && sudo journalctl --vacuum-size=200M && sudo journalctl --flush
+# /okay fine i'll use the wine ppa even though it's always fkn down
 
 # !!!
 # FINAL STEPS
